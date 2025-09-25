75a19a873026c8e3e3cdd4a926095899eeed932b
Seoeun Choi <holly101763@gmail.com>
2025-09-25 19:18:40 +0900

    first commit

---
+++ src/main/java/com/pikcurchu/pikcur/PikcurApplication.java
@@ -0,0 +1,13 @@
+package com.pikcurchu.pikcur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikcurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikcurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pikcurchu/pikcur/PikcurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pikcurchu.pikcur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikcurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
