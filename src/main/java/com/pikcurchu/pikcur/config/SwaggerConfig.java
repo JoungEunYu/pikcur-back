27384c3e09e7c3ff687bcb762ad2aa702b879ebe
Seoeun Choi <holly101763@gmail.com>
2025-09-25 20:30:37 +0900

    feat:add swagger

---
+++ src/main/java/com/pikcurchu/pikcur/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.pikcurchu.pikcur.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PIKCUR API 문서") // 문서 제목
+                        .description("PIKCUR의 REST API 명세서입니다.") // 문서 설명
+                        .version("v1.0.0")); // API 버전
+    }
+}
